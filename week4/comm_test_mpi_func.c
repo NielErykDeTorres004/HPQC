0e713b829a2afe84107e052a5eb5894e29454f03
Niel <niel.detorres2@mail.dcu.ie>
2026-02-16 13:26:27 +0000

    Functionalise comm_test_mpi into root/client tasks

--- week4/comm_test_mpi_func.c
+++ week4/comm_test_mpi_func.c
@@ -23,12 +23,6 @@ int main(int argc, char **argv)
 	ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
 	ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
 
-	// creates and initialies transmission variables
-	int send_message, recv_message, count, dest, source, tag;
-	send_message = recv_message = dest = source = tag = 0;
-	count = 1;
-	MPI_Status status;
-
 	check_uni_size(uni_size);
 	check_task(uni_size, my_rank);
 
@@ -40,11 +34,49 @@ int main(int argc, char **argv)
 
 void root_task(int uni_size)
 {
+        // creates and initialies transmission variables
+        int recv_message, count, source, tag;
+        recv_message = source = tag = 0;
+        count = 1;
+        MPI_Status status;
+
+        // iterates through all the other ranks
+        for (int their_rank = 1; their_rank < uni_size; their_rank++)
+        {
+                // sets the source argument to the rank of the sender
+                source = their_rank;
+
+                // receives the messages
+                MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+
+                // prints the message from the sender
+                printf("Hello, I am %d of %d. Received %d from Rank %d\n",
+                        0, uni_size, recv_message, source);
+
+        } // end for (int their_rank = 1; their_rank < uni_size; their_rank++)
 }
 
 void client_task(int my_rank, int uni_size)
 {
+        // creates and initialies transmission variables
+        int send_message, count, dest, tag;
+        send_message = dest = tag = 0;
+        count = 1;
+
+        // sets the destination for the message
+        dest = 0; // destination is root
+
+        // creates the message
+        send_message = my_rank * 10;
+
+        // sends the message
+        MPI_Send(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
+
+        // prints the message from the sender
+        printf("Hello, I am %d of %d. Sent %d to Rank %d\n",
+                my_rank, uni_size, send_message, dest);
 }
+
 
 void check_uni_size(int uni_size)
 {
@@ -66,4 +98,12 @@ void check_uni_size(int uni_size)
 
 void check_task(int uni_size, int my_rank)
 {
+	if (0 == my_rank)
+	{
+		root_task(uni_size);
+	}
+	else
+	{
+		client_task(my_rank, uni_size);
+	}
 }
